e0e0a9940535b46870fc56c16e2b7d30afb7d11b
Alexandru Crisan <alexandru.crisan@cpp.canon>
2025-03-03 16:28:46 +0200

    login and register phase1

--- PiIrrigateWeb/ClientApp/src/app/app.routes.ts
+++ PiIrrigateWeb/ClientApp/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
+    {path: 'landing', component: LandingComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent},
     {path: '', component: LandingComponent},
     {path: '**', component: LandingComponent},
 ];
